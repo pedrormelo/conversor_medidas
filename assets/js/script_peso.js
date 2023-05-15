4c882e4868ba0049f5ec3fda838532d8b9f0bfe0
pedrormelo <sakki.backup@gmail.com>
2023-05-14 23:20:22 -0300

    fixed js for peso

--- assets/js/script_peso.js
+++ assets/js/script_peso.js
@@ -1,22 +1,31 @@
-const { pesos } = require("peso.json")
-
 const form = document.querySelector('.form-convert');
+const input = document.querySelector('#value1');
+const select1 = document.querySelector('#unit_one');
+const select2 = document.querySelector('#unit_two');
+const button = document.querySelector('#convert');
+const resultadoElement = document.querySelector('#resultado');
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
+const proporcoes = {
+  kg: 1,
+  lb: 0.453592,
+  gramas: 0.001,
+  mg: 0.000001,
+  toneladas: 1000
+};
 
-  const valorEntrada = document.querySelector('#value1').value;
-  const unidade1 = document.querySelector('#unit_one').value;
-  const unidade2 = document.querySelector('#unit_two').value;
-  const resultado = document.querySelector('#resultado');
+function converter(valor, unidade1, unidade2) {
+  const proporcao1 = proporcoes[unidade1];
+  const proporcao2 = proporcoes[unidade2];
+  return valor * proporcao1 / proporcao2;
+}
 
-  let valorSaida;
+function handleSubmit(event) {
+  event.preventDefault();
+  const valor = parseFloat(input.value);
+  const unidade1 = select1.value;
+  const unidade2 = select2.value;
+  const resultado = converter(valor, unidade1, unidade2);
+  resultadoElement.textContent = `${valor} ${unidade1} = ${resultado.toFixed(2)} ${unidade2}`;
+}
 
-  pesos.map(peso => console.log(peso.nome_medida))
-  
-  function converterPeso() {
-    const 
-  }
-
-  resultado.value = valorSaida;
-});
+form.addEventListener('submit', handleSubmit);
